8a1459f5d432bd0eb65d0d796ec0c522906375e5
Andrew Henry <akhenry@gmail.com>
2022-02-24 09:44:56 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'plugin.js',
-        library: '[name]',
+        library: 'OpenMCTLegacyPlugin',
         libraryTarget: 'umd',
     },
     
